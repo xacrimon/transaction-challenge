ae2e1f277b7420fab1151fd6ba9962c678e429e2
Joel Wejdenstål <jwejdenstal@icloud.com>
2022-03-20 01:41:08 +0100

    handler dispatch

--- src/engine.rs
+++ src/engine.rs
@@ -23,6 +23,32 @@ impl Engine {
     }
 
     pub fn apply(&mut self, tx: Tx) {
+        match tx.ty {
+            TxType::Deposit => self.deposit(tx.client, tx.amount, tx.id),
+            TxType::Withdrawal => self.withdraw(tx.client, tx.amount, tx.id),
+            TxType::Dispute => self.dispute(tx.client, tx.amount, tx.id),
+            TxType::Resolve => self.resolve(tx.client, tx.amount, tx.id),
+            TxType::Chargeback => self.chargeback(tx.client, tx.amount, tx.id),
+        }
+    }
+
+    fn deposit(&mut self, client: Client, amount: Amount, tx_id: TxId) {
+        todo!()
+    }
+
+    fn withdraw(&mut self, client: Client, amount: Amount, tx_id: TxId) {
+        todo!()
+    }
+
+    fn dispute(&mut self, client: Client, amount: Amount, tx_id: TxId) {
+        todo!()
+    }
+
+    fn resolve(&mut self, client: Client, amount: Amount, tx_id: TxId) {
+        todo!()
+    }
+
+    fn chargeback(&mut self, client: Client, amount: Amount, tx_id: TxId) {
         todo!()
     }
 
